4b9e1ed36e331d7d07e47abbc15d3255a9670377
chenghui03 <147025572+chenghui03@users.noreply.github.com>
2025-10-15 12:25:25 +0800

    Update quartz.config.ts
    baseUrl change to homepage

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "",
+    baseUrl: "chenghui03.github.io/HomePage",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
